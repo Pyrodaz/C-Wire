aa20d62e2a6e58d623614745ccbe99e22b87432a
Pyrodaz <arcol1807@gmail.com>
2024-12-09 15:59:36 +0100

    Verification des arguments dans le bash + s'il y a -h pour afficher le menu help + creation du fichier csv avec les résultats (pas de traitement pour le moment et vérification si le fichier exec existe (fichier de test dans dossier codeC)

--- c-wire.sh
+++ c-wire.sh
@@ -2,10 +2,28 @@
 
 help=0
 
-#check first arg is a file
-if ! [ -f "$1" ]; then
-	echo "'$1' is not a file"
-	help=1; #Display help menu
+#check if there is -h arg in args to display help menu. Other option are invalidate 
+
+for arg in $*; do 
+	if [ "$arg" == "-h" ]; then 
+		echo "-h OK"
+		help=1
+	fi
+done
+
+if [ $help -eq 0 ]; then
+	if [ "$#" -ne 3 ] && [ "$#" -ne 4 ]; then 
+		echo "bad number of arguments"
+		help=1
+	fi
+fi
+
+if [ $help -eq 0 ]; then
+	#check first arg is a file
+	if ! [ -f "$1" ]; then
+		echo "'$1' is not a file"
+		help=1 #Display help menu
+	fi
 fi
 
 #check if next args are valid ; if one condition is wrong display help menu and don't check other conditions
@@ -13,7 +31,7 @@ fi
 if [ $help -eq 0 ]; then
 	if [ "$2" != "hvb" ] && [ "$2" != "hva" ] && [ "$2" != "lv" ]; then
 		echo "'$2' is not a valid argument"
-		help=1;
+		help=1
 	fi
 fi
 
@@ -21,7 +39,7 @@ fi
 if [ $help -eq 0 ]; then 
 	if [ "$3" != "all" ] && [ "$3" != "indiv" ] && [ "$3" != "comp" ]; then
 		echo "'$3 is not a valid argument"
-		help=1;
+		help=1
 	fi
 fi
 
@@ -29,16 +47,64 @@ if [ $help -eq 0 ]; then
 	if [ "$2" == "hvb" ] || [ "$2" == "hva" ]; then
 		if [ "$3" == "indiv" ] || [ "$3" == "all" ]; then
 			echo "'$2 $3' is not a valid enter"
-			help=1;
+			help=1
 		fi
 	fi
 fi
 
+#help menu and error menu if error in args
+if [ $help -eq 1 ]; then
+	echo "HELP"
+	#TO DO : write and put commands and explications to the help menu
+	exit 1
+fi
 
-#Suppression et recréation des fichiers tmp et graphs
+
+#Check if executable exist. TO CHANGE WHEN EXEC FILE NAME IS FIXED
+
+exec_exist=0
+files=`ls codeC`
+
+for file in $files; do
+	if [ $file == "exec" ]; then 
+		exec_exist=1
+	fi 
+done	
+
+
+
+#Remove and create tmp and graphs directories
 
 rm -rf "tmp"
 rm -rf "graphs"
 mkdir "tmp"
 mkdir "graphs"
+
+#Set up the chronometer to mesure execution time of the program
+
+SECONDS="0"
+
+#Create file to display the consommation of hva,hvb or lv station 
+
+case $# in 
+	3) touch "$2_$3.csv"
+		echo "$2_$3.csv";;
+	4) touch "$2_$3_$4.csv"
+		echo "$2_$3_$4.csv";;  
+	*) ;;
+esac 
+
+#RETIRE IN FINAL VERSION
+rm -f h*.csv
+rm -f lv*.csv
+
+
+#print program's execution time
+
+echo $SECONDS
+
+
+
+
+
 
