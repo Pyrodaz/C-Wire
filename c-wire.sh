3f4ebf64ec070e6c96d17387e9aead7090f3cfe9
Pyrodaz <arcol1807@gmail.com>
2024-12-21 23:36:50 +0100

    Ajout commentaires + graphique quand on sélectionne une centrale pour lv all. Vérification du makefile

--- c-wire.sh
+++ c-wire.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-SECONDS="0"
+
 help=0
 
 #check if there is -h arg in args to display help menu. Other option are invalidate 
 
 for arg in $*; do 
 	if [ "$arg" == "-h" ]; then 
-		echo "-h OK"
+		echo "Displaying help"
 		help=1
 	fi
 done
@@ -30,9 +30,10 @@ if [ $help -eq 0 ]; then
 	fi
 fi
 
-#check if next args are valid ; if one condition is wrong display help menu and don't check other conditions
+#check if next args are valid ; if one condition is wrong it displays help menu and don't check other conditions
 
 if [ $help -eq 0 ]; then
+	#Check if the type of station is valid
 	if [ "$2" != "hvb" ] && [ "$2" != "hva" ] && [ "$2" != "lv" ]; then
 		echo "'$2' is not a valid argument"
 		help=1
@@ -41,6 +42,7 @@ fi
 
 
 if [ $help -eq 0 ]; then 
+	#Check if the type of component is valid
 	if [ "$3" != "all" ] && [ "$3" != "indiv" ] && [ "$3" != "comp" ]; then
 		echo "'$3 is not a valid argument"
 		help=1
@@ -48,6 +50,7 @@ if [ $help -eq 0 ]; then
 fi
 
 if [ $help -eq 0 ]; then
+	#Check the coherence of the type of component and the type of station
 	if [ "$2" == "hvb" ] || [ "$2" == "hva" ]; then
 		if [ "$3" == "indiv" ] || [ "$3" == "all" ]; then
 			echo "'$2 $3' is not a valid enter"
@@ -76,7 +79,7 @@ fi
 
 #help menu and error menu if error in args
 if [ $help -eq 1 ]; then
-	echo "HELP"
+	echo "HELP MENU"
 	cat README.md
 	echo "Program's execution time : 0 sec"
 	exit 1
@@ -101,30 +104,55 @@ fi
 mkdir tmp
 mkdir graphs
 
-rm -f hv*.csv
-rm -f lv*.csv
+main_exist=0
+files=`ls codeC`
+file_counter=0 #Count .c file to verify later if there is the right number of .o file and if compilation worked
+
+for file in $files; do
+	if [ $file == "main" ]; then 
+		main_exist=1
+	fi
+	
+	if [[ $file == *.c ]]; then 
+		file_counter=$(($file_counter + 1))
+	fi 
+done	
+
+if [ $main_exist -eq 0 ];then
+	cd CodeC
+	make
+	#Update codeC directory to search the .o file and decrement the count to 0 if the compilation worked
+	files_C_compiled=`ls codeC` 
+	for file in $files_C_compiled; do 
+		if [ $file == "*.o" ]; then 
+			file_counter=$(($file_counter - 1))
+		fi 
+	done
+	echo "$file_counter"
+	if [ $file_counter -ne 0 ]; then
+		echo "Compilation failed"
+		exit 2
+	fi
+	cd ..
+fi
 
 cp "$1" "input/inputfile.csv"
 
 #Set up the chronometer to mesure execution time of the program
 
+SECONDS="0"
 
-
-#Create file to display the consommation of hva,hvb or lv station 
+#Create file to display the global consumption of hva,hvb or lv station 
 
 
 
 case $# in 
-	3)	touch "tests/$2_$3.csv"
-		csv_result="$2_$3.csv"
-		echo "$2_$3.csv";;
-	4)	touch "tests/$2_$3_$4.csv"
-		csv_result="$2_$3_$4.csv"
-		echo "$2_$3_$4.csv";;  
+	3)	touch "tests/$2_$3.csv";;
+	4)	touch "tests/$2_$3_$4.csv";;
 	*) 	;;
 esac 
 
-
+#Creation of the file data_to_process.csv to sort data before processing it into C program
 
 touch tmp/data_to_process.csv
 
@@ -133,6 +161,7 @@ case $2 in
 	#For hva and hvb there is only the comp case
 	hva)
 		if [ $# -eq 4 ]; then
+			#If there is a power plant selected we only take selected components and stations from this power plant
 			cat $1 | tail -n+2 | grep -E "^$4;[^;]*;[0-9]+;-;" | cut -d ";" -f3,7,8 | sort -t';' -n -k3 | tr "-" "0" >> tmp/data_to_process.csv
 			
 		else
@@ -182,6 +211,7 @@ case $2 in
 	*);;
 esac  
 
+#Creation of data_process.csv (Explications below). chmod is used to write into the file later.
 
 touch tmp/data_processed.csv
 chmod 777 tmp/data_processed.csv
@@ -189,10 +219,18 @@ chmod 777 tmp/data_processed.csv
 
 
 #Create the final file
-#SUPPLEMENTARY STEPS TO DO LATER WHEN C PROGRAM IS FINISHED Line 239
+
+: '
+We Send sorted data in data_to_process.csv to C program to calculate global 
+consumption of all components selected of the type of station selected
+to write it along with the ID of the type of stations and their capacity
+in a new csv file : data_processed.csv
+'
+
+#Checking compilation
 
 cd codeC
-make 
+chmod 777 main
 datapath="tmp/data_to_process.csv"
 ./main "../$datapath" "../tmp/data_processed.csv"
 
@@ -203,13 +241,13 @@ case $2 in
 	hva)
 		if [ $# -eq 4 ]; then
 		
-		echo "HV-A:Capacity:Load" > "tests/$2_$3_$4.csv"
-		sort tmp/data_processed.csv -t':' -n -k2 >> "tests/$2_$3_$4.csv"
+			echo "HV-A:Capacity:Load" > "tests/$2_$3_$4.csv"
+			sort tmp/data_processed.csv -t':' -n -k2 >> "tests/$2_$3_$4.csv"
 		
 		else 
 		
-		echo "HV-A:Capacity:Load" > "tests/$2_$3.csv"
-		sort tmp/data_processed.csv -t':' -n -k2 >> "tests/$2_$3.csv"
+			echo "HV-A:Capacity:Load" > "tests/$2_$3.csv"
+			sort tmp/data_processed.csv -t':' -n -k2 >> "tests/$2_$3.csv"
 		
 		fi;;
 	
@@ -218,13 +256,13 @@ case $2 in
 	
 		if [ $# -eq 4 ]; then
 		
-		echo "HV-B:Capacity:Load" > "tests/$2_$3_$4.csv"
-		sort tmp/data_processed.csv -t':' -n -k2 >> "tests/$2_$3_$4.csv"
+			echo "HV-B:Capacity:Load" > "tests/$2_$3_$4.csv"
+			sort tmp/data_processed.csv -t':' -n -k2 >> "tests/$2_$3_$4.csv"
 		
 		else 
 	
-		echo "HV-B:Capacity:Load" > "tests/$2_$3.csv"
-		sort "tmp/data_processed.csv" -t':' -n -k2 >> "tests/$2_$3.csv"
+			echo "HV-B:Capacity:Load" > "tests/$2_$3.csv"
+			sort "tmp/data_processed.csv" -t':' -n -k2 >> "tests/$2_$3.csv"
 		
 		fi;;
 		
@@ -232,40 +270,43 @@ case $2 in
 		
 		if [ $# -eq 4 ]; then
 		
-		echo "LV:Capacity:Load" > "tests/$2_$3_$4.csv"
-		sort "tmp/data_processed.csv" -t':' -n -k2 >> "tests/$2_$3_$4.csv" 
-		fileLV="$2_$3_$4" 
+			echo "LV:Capacity:Load" > "tests/$2_$3_$4.csv"
+			sort "tmp/data_processed.csv" -t':' -n -k2 >> "tests/$2_$3_$4.csv" 
+			fileLV="$2_$3_$4" 
 		
 		else 
 	
-		echo "LV:Capacity:Load" > "tests/$2_$3.csv"
-		sort "tmp/data_processed.csv" -t':' -n -k2 >> "tests/$2_$3.csv"
-		fileLV="$2_$3" 
+			echo "LV:Capacity:Load" > "tests/$2_$3.csv"
+			sort "tmp/data_processed.csv" -t':' -n -k2 >> "tests/$2_$3.csv"
+			fileLV="$2_$3" 
 
 		fi
 
 		if [ "$3" == "all" ]; then
+			#In the case of lv all, we create a file minmax with the ten station with the least and the ten with the most consumption
 			tail -n+2 "tests/$fileLV.csv" | sort -t':' -n -k3 > "tmp/sortload.csv"
 			head -n10 "tmp/sortload.csv" >> "tmp/sortdiff.csv"
 			tail -n10 "tmp/sortload.csv" >> "tmp/sortdiff.csv"
 			echo "LV:Capacity:Load:Energy Difference" > tests/"$fileLV"_minmax.csv
 			awk -F: '{$4=$2-$3} {print $0}' OFS=: "tmp/sortdiff.csv" | sort -t':' -n -k4 >> tests/"$fileLV"_minmax.csv
 			
-			#Bar graphic
-				cd tests
-				gnuplot -persist <<-EOFMarker
-					set xlabel "LV"
-					set ylabel "Capacity/Load"
-					set grid
-					set datafile separator ":"
-					set boxwidth 2.0
-					set style data histogram
-					set style fill solid
-					set terminal png
-					set output "../graphs/lvgraphs.png"
-					plot "lv_all_minmax.csv" using 1:2 with boxes linecolor rgb "#00FF00" title "Capacity", \
-						 "lv_all_minmax.csv" using 1:3 with boxes linecolor rgb "#FF0000" title "Load"
-				EOFMarker
+			#Program to create a graph of the file lv_all_minmax.csv in the case of lv all option
+			fileLVminmax="$fileLV"_minmax.csv
+			
+			cd tests
+			gnuplot -persist <<-EOFMarker
+				set xlabel "LV"
+				set ylabel "Capacity/Load"
+				set grid
+				set datafile separator ":"
+				set boxwidth 2.0
+				set style data histogram
+				set style fill solid
+				set terminal png
+				set output "../graphs/lvgraphs.png"
+				plot "$fileLVminmax" using 1:2 with boxes linecolor rgb "#00FF00" title "Capacity", \
+					 "$fileLVminmax" using 1:3 with boxes linecolor rgb "#FF0000" title "Load"
+			EOFMarker
 			
 
 		fi;;
--- codeC/CreationArbreAVL.h
+++ codeC/CreationArbreAVL.h
@@ -21,19 +21,19 @@ typedef struct _tree{
     int balance;
 } Tree;
  
-int min2(int a, int b);
-int max2(int a, int b);
-int min3(int a, int b, int c);
-int max3(int a, int b, int c);
-Tree* createAVL(Station station);
-Tree* rotateLeft(Tree* pRoot);
-Tree* rotateRight(Tree* pRoot);
-Tree* doubleRotateLeft(Tree* pRoot);
-Tree* doubleRotateRight(Tree* pRoot);
-Tree* balanceAVL(Tree* pRoot);
-Tree* insertAVL(Tree* pTree, Station station, int* h);
-Tree* freeAVL(Tree* pTree);
-void infix_write(Tree* pTree, FILE* file);
+int min2(int a, int b); //Calculate the minimum between 2 numbers
+int max2(int a, int b); //Calculate the maximum between 2 numbers
+int min3(int a, int b, int c); //Calculate the minimum between 3 numbers
+int max3(int a, int b, int c); //Calculate the minimum between 3 numbers
+Tree* createAVL(Station station); //Create an AVL Tree
+Tree* rotateLeft(Tree* pRoot); //Rotate the tree to the left to balance the AVL tree with pRoot's right son as pivot
+Tree* rotateRight(Tree* pRoot);//Rotate the tree to the right to balance the AVL tree with pRoot's left son as pivot
+Tree* doubleRotateLeft(Tree* pRoot); //Rotate the tree to the right than the left to balance the AVL tree
+Tree* doubleRotateRight(Tree* pRoot); //Rotate the tree to the left than the right to balance the AVL tree
+Tree* balanceAVL(Tree* pRoot); //Use the rotation to balance the AVL tree
+Tree* insertAVL(Tree* pTree, Station station, int* h); //Insert a station into a AVL tree with a pointer on its root pTree
+Tree* freeAVL(Tree* pTree); //Free the memory which is taken by an AVL tree recursively
+void infix_write(Tree* pTree, FILE* file); //Write the data in the station of the AVL tree in a csv file
 
 
 #endif 
